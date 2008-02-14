c7f9b9775dede4e7fc6ed1926e66192977d2c821
Mickaël Guessant <mguessan@free.fr>
2008-02-14 15:51:34 +0000

    added parameter to davmail.sh script for davmail.properties
    git-svn-id: https://svn.code.sf.net/p/davmail/code/trunk@129 3d1905a2-6b24-0410-a738-b14d5a86fcbd

--- davmail.sh
+++ davmail.sh
@@ -3,4 +3,4 @@
 # sudo apt-get install sun-java6-bin
 # launch davmail :
 for i in lib/*; do export CLASSPATH=$CLASSPATH:$i; done
-java -cp davmail.jar:$CLASSPATH davmail.DavGateway
+java -cp davmail.jar:$CLASSPATH davmail.DavGateway $1
