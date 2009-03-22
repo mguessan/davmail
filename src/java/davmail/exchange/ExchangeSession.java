96c3bf8ff52a17c6e210bea11e584d8b43859ff7
Mickaël Guessant <mguessan@free.fr>
2009-03-22 19:22:27 +0000

    Caldav: retrieve VTODO events created in the calendar folder, even if they remain invisible in OWA and Outlook
    git-svn-id: https://svn.code.sf.net/p/davmail/code/trunk@474 3d1905a2-6b24-0410-a738-b14d5a86fcbd

--- src/java/davmail/exchange/ExchangeSession.java
+++ src/java/davmail/exchange/ExchangeSession.java
@@ -1189,7 +1189,9 @@ public class ExchangeSession {
 
         String searchQuery = "Select \"DAV:getetag\"" +
                 "                FROM Scope('SHALLOW TRAVERSAL OF \"" + calendarUrl + "\"')\n" +
-                "                WHERE (\"urn:schemas:calendar:instancetype\" = 1\n" +
+                "                WHERE (" +
+                "                       \"urn:schemas:calendar:instancetype\" is null OR" +
+                "                       \"urn:schemas:calendar:instancetype\" = 1\n" +
                 "                OR (\"urn:schemas:calendar:instancetype\" = 0\n" +
                 dateCondition +
                 "                )) AND \"DAV:contentclass\" = 'urn:content-classes:appointment'\n" +
