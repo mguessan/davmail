906f531132d07b78852381ab9cb6ef9451508185
Mickaël Guessant <mguessan@free.fr>
2010-06-07 21:19:09 +0000

    EWS: implement IndexedFieldURI and InternetMessageHeader
    git-svn-id: https://svn.code.sf.net/p/davmail/code/trunk@1079 3d1905a2-6b24-0410-a738-b14d5a86fcbd

---
+++ src/java/davmail/exchange/ews/IndexedFieldURI.java
@@ -0,0 +1,38 @@
+/*
+ * DavMail POP/IMAP/SMTP/CalDav/LDAP Exchange Gateway
+ * Copyright (C) 2010  Mickael Guessant
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package davmail.exchange.ews;
+
+/**
+ * Indexed FieldURI
+ */
+public class IndexedFieldURI implements FieldURI {
+    protected String fieldURI;
+    protected String fieldIndex;
+
+    public IndexedFieldURI(String fieldURI, String fieldIndex) {
+        this.fieldURI = fieldURI;
+        this.fieldIndex = fieldIndex;
+    }
+
+    public void appendTo(StringBuilder buffer) {
+        buffer.append("<t:IndexedFieldURI FieldURI=\"").append(fieldURI);
+        buffer.append("\" FieldIndex=\"").append(fieldIndex);
+        buffer.append("\"/>");
+    }
+}
