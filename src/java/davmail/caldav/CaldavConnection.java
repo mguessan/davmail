37e8aa137d89a49895097880dc73fdddac4f961e
Mickaël Guessant <mguessan@free.fr>
2010-07-27 21:23:56 +0000

    Caldav: reenable Lightning 1.0b2 bug workaround
    git-svn-id: https://svn.code.sf.net/p/davmail/code/trunk@1308 3d1905a2-6b24-0410-a738-b14d5a86fcbd

--- src/java/davmail/caldav/CaldavConnection.java
+++ src/java/davmail/caldav/CaldavConnection.java
@@ -1318,7 +1318,7 @@ public class CaldavConnection extends AbstractConnection {
         }
 
         protected boolean isBrokenHrefEncoding() {
-            return isUserAgent("DAVKit/3") || isUserAgent("eM Client/")/* || isUserAgent("Lightning/1.0b2")*/;
+            return isUserAgent("DAVKit/3") || isUserAgent("eM Client/") || isUserAgent("Lightning/1.0b2");
         }
 
         protected boolean isLightning() {
