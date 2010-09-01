fe4bd3070a49d23395200e3027f29482b8448fa7
Mickaël Guessant <mguessan@free.fr>
2010-09-01 06:46:46 +0000

    SMTP: Improve message on MAIL FROM without authentication
    git-svn-id: https://svn.code.sf.net/p/davmail/code/trunk@1403 3d1905a2-6b24-0410-a738-b14d5a86fcbd

--- src/java/davmail/smtp/SmtpConnection.java
+++ src/java/davmail/smtp/SmtpConnection.java
@@ -123,6 +123,8 @@ public class SmtpConnection extends AbstractConnection {
                             state = State.STARTMAIL;
                             recipients.clear();
                             sendClient("250 Sender OK");
+                        } else if (state == State.INITIAL) {
+                            sendClient("503 Authentication required");
                         } else {
                             state = State.INITIAL;
                             sendClient("503 Bad sequence of commands");
