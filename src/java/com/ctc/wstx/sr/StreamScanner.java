1a09cd237f3e79cce91cf7d456d8a801d483f22e
Mickaël Guessant <mguessan@free.fr>
2014-03-10 22:20:26 +0000

    Prepare WoodStox Xml10AllowAllEscapedChars setting implementation
    git-svn-id: https://svn.code.sf.net/p/davmail/code/trunk@2251 3d1905a2-6b24-0410-a738-b14d5a86fcbd

--- src/java/com/ctc/wstx/sr/StreamScanner.java
+++ src/java/com/ctc/wstx/sr/StreamScanner.java
@@ -276,6 +276,12 @@ public abstract class StreamScanner
      */
     protected boolean mNormalizeLFs;
 
+    /**
+     * Flag that indicates whether all escaped chars are accepted in XML 1.0.
+     */
+    protected boolean mXml10AllowAllEscapedChars = true;
+
+
     /*
     ///////////////////////////////////////////////////////////
     // Buffer(s) for local name(s) and text content
@@ -2383,11 +2389,13 @@ public abstract class StreamScanner
             if (value == 0) {
                 throwParseError("Invalid character reference: null character not allowed in XML content.");
             }
-            // XML 1.1 allows most other chars; 1.0 does not: However Exchange sends such chars with XML 1.0
-            //if (!mXml11 &&
-            //    (value != 0x9 && value != 0xA && value != 0xD)) {
-            //    reportIllegalChar(value);
-            //}
+            // XML 1.1 allows most other chars; 1.0 does not:
+            if (!mXml10AllowAllEscapedChars) {
+                if (!mXml11 &&
+                    (value != 0x9 && value != 0xA && value != 0xD)) {
+                    reportIllegalChar(value);
+                }
+            }
         }
     }
 
