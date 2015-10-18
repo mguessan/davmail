78558f8010147f55d1c4245f0c02a6b36a9be162
Mickaël Guessant <mguessan@free.fr>
2015-10-18 09:53:11 +0000

    Change warning messages to debug in close method
    git-svn-id: https://svn.code.sf.net/p/davmail/code/trunk@2395 3d1905a2-6b24-0410-a738-b14d5a86fcbd

--- src/java/davmail/AbstractConnection.java
+++ src/java/davmail/AbstractConnection.java
@@ -250,20 +250,20 @@ public class AbstractConnection extends Thread {
             try {
                 in.close();
             } catch (IOException e2) {
-                DavGatewayTray.warn(new BundleMessage("LOG_EXCEPTION_CLOSING_CLIENT_INPUT_STREAM"), e2);
+                DavGatewayTray.debug(new BundleMessage("LOG_EXCEPTION_CLOSING_CLIENT_INPUT_STREAM"), e2);
             }
         }
         if (os != null) {
             try {
                 os.close();
             } catch (IOException e2) {
-                DavGatewayTray.warn(new BundleMessage("LOG_EXCEPTION_CLOSING_CLIENT_OUTPUT_STREAM"), e2);
+                DavGatewayTray.debug(new BundleMessage("LOG_EXCEPTION_CLOSING_CLIENT_OUTPUT_STREAM"), e2);
             }
         }
         try {
             client.close();
         } catch (IOException e2) {
-            DavGatewayTray.warn(new BundleMessage("LOG_EXCEPTION_CLOSING_CLIENT_SOCKET"), e2);
+            DavGatewayTray.debug(new BundleMessage("LOG_EXCEPTION_CLOSING_CLIENT_SOCKET"), e2);
         }
     }
 }
