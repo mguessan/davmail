842652a403e53c6c37761f535db15f8107d065d5
Mickaël Guessant <mguessan@free.fr>
2016-07-26 18:20:05 +0000

    update icon cache
    git-svn-id: https://svn.code.sf.net/p/davmail/code/trunk@2431 3d1905a2-6b24-0410-a738-b14d5a86fcbd

--- src/winrun4j/update.cmd
+++ src/winrun4j/update.cmd
@@ -18,3 +18,5 @@ rcedit64 /N davmail64.exe davmail64.ini
 rcedit64 /C davmailservice64.exe
 rcedit64 /I davmailservice64.exe davmail.ico
 rcedit64 /N davmailservice64.exe davmailservice64.ini
+
+ie4uinit.exe -show
