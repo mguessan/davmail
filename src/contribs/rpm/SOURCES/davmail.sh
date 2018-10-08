3cf950065d397eb8aac6c5f6d1f598e5760c46b5
Mickaël Guessant <mguessan@free.fr>
2018-10-08 20:17:23 +0000

    Linux: merge davmail.sh to use a single script in all cases
    git-svn-id: https://svn.code.sf.net/p/davmail/code/trunk@2706 3d1905a2-6b24-0410-a738-b14d5a86fcbd

--- src/contribs/rpm/SOURCES/davmail.sh
+++ src/contribs/rpm/SOURCES/davmail.sh
@@ -2,18 +2,17 @@
 #
 # Usage: davmail [</path/to/davmail.properties>]
 #
-
-# Create the $HOME/.davmail.properties if necessary
-if [ -r /etc/davmail.properties ]; then
-  if [ ! -f ${HOME}/.davmail.properties ]; then
-	grep -v ^davmail.logFilePath /etc/davmail.properties | \
-	  sed -e 's/^davmail.server=true/davmail.server=false/' > \
-	    ${HOME}/.davmail.properties
-  fi
+# In case of SWT crash under JDK 9, uninstall SWT or remove the second case below
+#
+BASE=`dirname $0`
+JAVA_OPTS="-Xmx512M -Dsun.net.inetaddr.ttl=60"
+if [ -f $BASE/davmail.jar ]; then
+    # this is the platform independent package
+    exec java $JAVA_OPTS -cp davmail.jar:lib/* davmail.DavGateway "$@"
+elif [ -f /usr/share/java/swt.jar ]; then
+    # standard install with SWT
+    export LD_LIBRARY_PATH=/usr/lib/jni
+    exec java $JAVA_OPTS -cp /usr/share/davmail/davmail.jar:/usr/share/java/swt.jar:/usr/share/davmail/lib/* davmail.DavGateway "$@"
+else
+    exec java $JAVA_OPTS -cp /usr/share/davmail/davmail.jar:/usr/share/davmail/lib/* davmail.DavGateway "$@"
 fi
-
-# Add our libs into CLASSPATH
-for i in /usr/share/davmail/lib/*; do export CLASSPATH=${CLASSPATH}:${i}; done
-
-# Start davmail
-java -cp /usr/share/davmail/davmail.jar:${CLASSPATH} davmail.DavGateway $*
