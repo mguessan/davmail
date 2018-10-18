394d5e85462356ae4b5ac64bb116b6634afa9530
Mickaël Guessant <mguessan@free.fr>
2018-10-18 07:41:54 +0000

    Linux: Drop old davmail.sh script
    git-svn-id: https://svn.code.sf.net/p/davmail/code/trunk@2753 3d1905a2-6b24-0410-a738-b14d5a86fcbd

--- davmail.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Ubuntu setup instructions :
-# install java :
-# sudo apt-get install default-jre
-# launch davmail
-BASE=`dirname $0`
-for i in $BASE/lib/*; do export CLASSPATH=$CLASSPATH:$i; done
-exec java -Xmx512M -Dsun.net.inetaddr.ttl=60 -cp $BASE/davmail.jar:$CLASSPATH davmail.DavGateway $1
