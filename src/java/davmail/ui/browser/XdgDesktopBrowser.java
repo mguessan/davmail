eabef064dbf66459f5d47a41962ce075c7a5144c
Mickaël Guessant <mguessan@free.fr>
2018-10-24 17:45:10 +0000

    Merge DesktopBrowser: add support for xdg-open directly, see https://github.com/mguessan/davmail/pull/5
    git-svn-id: https://svn.code.sf.net/p/davmail/code/trunk@2763 3d1905a2-6b24-0410-a738-b14d5a86fcbd

---
+++ src/java/davmail/ui/browser/XdgDesktopBrowser.java
@@ -0,0 +1,41 @@
+/*
+ * DavMail POP/IMAP/SMTP/CalDav/LDAP Exchange Gateway
+ * Copyright (C) 2009  Mickael Guessant
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package davmail.ui.browser;
+
+import java.io.IOException;
+import java.net.URI;
+
+/**
+ * Failover: Runtime.exec open URL
+ */
+public final class XdgDesktopBrowser {
+    private XdgDesktopBrowser() {
+    }
+
+    /**
+     * Open default browser at location URI.
+     * Use xdg-open to open browser url
+     *
+     * @param location location URI
+     * @throws IOException on error
+     */
+    public static void browse(URI location) throws IOException {
+        Runtime.getRuntime().exec("xdg-open " + location.toString());
+    }
+}
