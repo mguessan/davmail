f8b0cbebb90f1fe566f0058eef47ee0980cd66ec
Mickaël Guessant <mguessan@free.fr>
2018-11-19 09:09:35 +0000

    EWS: cleanup O365 interactive
    git-svn-id: https://svn.code.sf.net/p/davmail/code/trunk@2794 3d1905a2-6b24-0410-a738-b14d5a86fcbd

--- src/java/davmail/exchange/auth/O365InteractiveAuthenticator.java
+++ src/java/davmail/exchange/auth/O365InteractiveAuthenticator.java
@@ -52,6 +52,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.net.*;
+import java.util.List;
 
 public class O365InteractiveAuthenticator extends JFrame implements ExchangeAuthenticator {
 
@@ -306,8 +307,6 @@ public class O365InteractiveAuthenticator extends JFrame implements ExchangeAuth
         try {
             Settings.setDefaultSettings();
             //Settings.setLoggingLevel("httpclient.wire", Level.DEBUG);
-            Settings.setProperty("davmail.proxyHost", "proxy.free.fr");
-            Settings.setProperty("davmail.proxyPort", "3128");
 
             O365InteractiveAuthenticator authenticationFrame = new O365InteractiveAuthenticator();
             authenticationFrame.setUsername("");
