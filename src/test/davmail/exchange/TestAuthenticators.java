d1f230a41049713e5c1fb9ea530973bff1e83946
Mickaël Guessant <mguessan@free.fr>
2018-11-29 09:39:29 +0000

    EWS: new authenticator test cases
    git-svn-id: https://svn.code.sf.net/p/davmail/code/trunk@2819 3d1905a2-6b24-0410-a738-b14d5a86fcbd

---
+++ src/test/davmail/exchange/TestAuthenticators.java
@@ -0,0 +1,52 @@
+/*
+ * DavMail POP/IMAP/SMTP/CalDav/LDAP Exchange Gateway
+ * Copyright (C) 2010  Mickael Guessant
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package davmail.exchange;
+
+import davmail.AbstractDavMailTestCase;
+import davmail.Settings;
+
+import java.io.IOException;
+
+public class TestAuthenticators extends AbstractDavMailTestCase {
+
+    public void testEWSAuthenticator() throws IOException {
+        Settings.setProperty("davmail.mode", Settings.EWS);
+        ExchangeSessionFactory.checkConfig();
+        // try application password for MFA enabledusers
+        ExchangeSessionFactory.getInstance(Settings.getProperty("davmail.username"),
+                Settings.getProperty("davmail.app.password"));
+    }
+
+    public void testO365Authenticator() throws IOException {
+        Settings.setProperty("davmail.mode", Settings.O365);
+        ExchangeSessionFactory.checkConfig();
+        // try application password for MFA enabledusers
+        ExchangeSessionFactory.getInstance(Settings.getProperty("davmail.username"),
+                Settings.getProperty("davmail.app.password"));
+    }
+
+    public void testO365ModernAuthenticator() throws IOException {
+        Settings.setProperty("davmail.mode", Settings.O365_MODERN);
+        ExchangeSessionFactory.checkConfig();
+        // try application password for MFA enabledusers
+        ExchangeSessionFactory.getInstance(Settings.getProperty("davmail.username"),
+                Settings.getProperty("davmail.password"));
+    }
+}
