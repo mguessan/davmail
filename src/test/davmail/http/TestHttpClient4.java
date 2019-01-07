8fb6b3637c95674b5cc9b52d92a8feabeb2b43af
Mickaël Guessant <mguessan@free.fr>
2019-01-07 23:41:56 +0000

    HTTP: More HttpClient 4 test cases for authentication, proxy, redirects and URI handling
    git-svn-id: https://svn.code.sf.net/p/davmail/code/trunk@2907 3d1905a2-6b24-0410-a738-b14d5a86fcbd

--- src/test/davmail/http/TestHttpClient4.java
+++ src/test/davmail/http/TestHttpClient4.java
@@ -20,19 +20,31 @@
 package davmail.http;
 
 import davmail.AbstractDavMailTestCase;
-import org.apache.commons.httpclient.HttpStatus;
+import davmail.Settings;
+import org.apache.http.Header;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpStatus;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.client.utils.URIUtils;
 import org.apache.http.config.RegistryBuilder;
 import org.apache.http.config.SocketConfig;
 import org.apache.http.conn.socket.ConnectionSocketFactory;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.log4j.Level;
 
 import java.io.IOException;
+import java.net.URI;
 
 public class TestHttpClient4 extends AbstractDavMailTestCase {
     public void testBasicGetRequest() throws IOException {
@@ -107,6 +119,127 @@ public class TestHttpClient4 extends AbstractDavMailTestCase {
             CloseableHttpResponse response = httpClient.execute(httpget);
             try {
                 assertEquals(HttpStatus.SC_MOVED_TEMPORARILY, response.getStatusLine().getStatusCode());
+            } finally {
+                response.close();
+            }
+        } finally {
+            httpClient.close();
+        }
+    }
+
+    public void testBasicAuthentication() throws IOException {
+
+        RegistryBuilder<ConnectionSocketFactory> schemeRegistry = RegistryBuilder.create();
+
+        schemeRegistry.register("https", new SSLConnectionSocketFactory(new DavGatewaySSLSocketFactory(),
+                SSLConnectionSocketFactory.getDefaultHostnameVerifier()));
+
+        CredentialsProvider provider = new BasicCredentialsProvider();
+        UsernamePasswordCredentials credentials
+                = new UsernamePasswordCredentials(username, password);
+        provider.setCredentials(AuthScope.ANY, credentials);
+
+        PoolingHttpClientConnectionManager poolingHttpClientConnectionManager = new PoolingHttpClientConnectionManager(schemeRegistry.build());
+        HttpClientBuilder clientBuilder = HttpClientBuilder.create()
+                .disableRedirectHandling()
+                .setDefaultCredentialsProvider(provider)
+                .setConnectionManager(poolingHttpClientConnectionManager);
+        CloseableHttpClient httpClient = clientBuilder.build();
+        try {
+
+            HttpGet httpget = new HttpGet("https://outlook.office365.com/EWS/Exchange.asmx");
+            CloseableHttpResponse response = httpClient.execute(httpget);
+            try {
+                assertEquals(HttpStatus.SC_OK, response.getStatusLine().getStatusCode());
+                String responseString = new BasicResponseHandler().handleResponse(response);
+                System.out.println(responseString);
+            } finally {
+                response.close();
+            }
+        } finally {
+            httpClient.close();
+        }
+    }
+
+    public void testHttpProxy() throws IOException {
+        Settings.setLoggingLevel("org.apache.http.wire", Level.DEBUG);
+        Settings.setLoggingLevel("org.apache.http", Level.DEBUG);
+
+        String proxyHost = Settings.getProperty("davmail.proxyHost");
+        int proxyPort = Settings.getIntProperty("davmail.proxyPort");
+        HttpHost proxy = new HttpHost(proxyHost, proxyPort);
+        HttpClientBuilder clientBuilder = HttpClientBuilder.create();
+        clientBuilder.setProxy(proxy).setUserAgent(DavGatewayHttpClientFacade.IE_USER_AGENT);
+        CloseableHttpClient httpClient = clientBuilder.build();
+        try {
+            HttpGet httpget = new HttpGet("http://davmail.sourceforge.net/version.txt");
+            CloseableHttpResponse response = httpClient.execute(httpget);
+            try {
+                String responseString = new BasicResponseHandler().handleResponse(response);
+                System.out.println(responseString);
+            } finally {
+                response.close();
+            }
+        } finally {
+            httpClient.close();
+        }
+    }
+
+    public void testGetPath() throws IOException {
+        Settings.setLoggingLevel("org.apache.http.wire", Level.DEBUG);
+        Settings.setLoggingLevel("org.apache.http", Level.DEBUG);
+
+        HttpClientBuilder clientBuilder = HttpClientBuilder.create();
+        CloseableHttpClient httpClient = clientBuilder.build();
+        try {
+            String url = "http://davmail.sourceforge.net";
+            // get with host
+            HttpGet httpget = new HttpGet(url);
+            CloseableHttpResponse response = httpClient.execute(httpget);
+            try {
+                new BasicResponseHandler().handleResponse(response);
+            } finally {
+                response.close();
+            }
+
+            // get with path only
+            httpget = new HttpGet(URIUtils.resolve(httpget.getURI(), "/version.txt"));
+            response = httpClient.execute(httpget);
+            try {
+                String responseString = new BasicResponseHandler().handleResponse(response);
+                System.out.println(responseString);
+            } finally {
+                response.close();
+            }
+        } finally {
+            httpClient.close();
+        }
+    }
+
+    public void testFollowRedirects() throws IOException {
+        Settings.setLoggingLevel("org.apache.http.wire", Level.DEBUG);
+        Settings.setLoggingLevel("org.apache.http", Level.DEBUG);
+
+        HttpClientBuilder clientBuilder = HttpClientBuilder.create().disableRedirectHandling();
+        CloseableHttpClient httpClient = clientBuilder.build();
+        try {
+            HttpGet httpget = new HttpGet("https://outlook.office365.com/owa/");
+            CloseableHttpResponse response = httpClient.execute(httpget);
+            Header location;
+            try {
+                assertEquals(HttpStatus.SC_MOVED_TEMPORARILY, response.getStatusLine().getStatusCode());
+                location = response.getFirstHeader("Location");
+            } finally {
+                response.close();
+            }
+            assertNotNull(location);
+            URI targetUri = URIUtils.resolve(httpget.getURI(), location.getValue());
+            httpget = new HttpGet(targetUri);
+            response = httpClient.execute(httpget);
+            try {
+                assertEquals(HttpStatus.SC_OK, response.getStatusLine().getStatusCode());
+                String responseString = new BasicResponseHandler().handleResponse(response);
+                System.out.println(responseString);
             } finally {
                 response.close();
             }
