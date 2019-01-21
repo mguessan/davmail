14afb3cc192457aaadbdd8ce9c99858729a5e318
Mickaël Guessant <mguessan@free.fr>
2019-01-21 08:35:23 +0000

    HTTP: more Caldav test cases with HttpClientAdapter
    git-svn-id: https://svn.code.sf.net/p/davmail/code/trunk@2934 3d1905a2-6b24-0410-a738-b14d5a86fcbd

--- src/test/davmail/caldav/TestCaldavHttpClient4.java
+++ src/test/davmail/caldav/TestCaldavHttpClient4.java
@@ -25,6 +25,9 @@ import davmail.Settings;
 import davmail.exchange.ExchangeSession;
 import davmail.exchange.ExchangeSessionFactory;
 import davmail.http.HttpClientAdapter;
+import davmail.util.StringUtil;
+import org.apache.commons.httpclient.util.URIUtil;
+import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -33,7 +36,12 @@ import org.apache.http.entity.StringEntity;
 import org.apache.jackrabbit.webdav.*;
 import org.apache.jackrabbit.webdav.client.methods.BaseDavRequest;
 import org.apache.jackrabbit.webdav.client.methods.HttpPropfind;
+import org.apache.jackrabbit.webdav.client.methods.HttpProppatch;
+import org.apache.jackrabbit.webdav.property.DavPropertyName;
 import org.apache.jackrabbit.webdav.property.DavPropertyNameSet;
+import org.apache.jackrabbit.webdav.property.DavPropertySet;
+import org.apache.jackrabbit.webdav.property.DefaultDavProperty;
+import org.apache.jackrabbit.webdav.xml.Namespace;
 
 import java.io.IOException;
 import java.net.URI;
@@ -184,9 +192,7 @@ public class TestCaldavHttpClient4 extends AbstractDavMailTestCase {
             }
         };
         method.setEntity(new StringEntity(buffer, ContentType.create("text/xml", "UTF-8")));
-
-        MultiStatus multiStatus = httpClient.executeDavRequest(method);
-        MultiStatusResponse[] responses = multiStatus.getResponses();
+        httpClient.executeDavRequest(method);
     }
 
     public void testReportTasks() throws IOException, DavException {
@@ -209,11 +215,150 @@ public class TestCaldavHttpClient4 extends AbstractDavMailTestCase {
         };
         method.setEntity(new StringEntity(buffer, ContentType.create("text/xml", "UTF-8")));
 
-        MultiStatus multiStatus = httpClient.executeDavRequest(method);
-        MultiStatusResponse[] responses = multiStatus.getResponses();
+        httpClient.executeDavRequest(method);
     }
 
-    public void testInvalidDavRequest() throws IOException, DavException {
+    public void testReportEventsOnly() throws IOException, DavException {
+        String buffer = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
+                "<C:calendar-query xmlns:C=\"urn:ietf:params:xml:ns:caldav\" xmlns:D=\"DAV:\">" +
+                "<D:prop>" +
+                "<C:calendar-data/>" +
+                "</D:prop>" +
+                "<C:comp-filter name=\"VCALENDAR\">" +
+                "<C:comp-filter name=\"VEVENT\"/>" +
+                "</C:comp-filter>" +
+                "<C:filter>" +
+                "</C:filter>" +
+                "</C:calendar-query>";
+        BaseDavRequest method = new BaseDavRequest(URI.create("/users/" + session.getEmail() + "/calendar/")) {
+            @Override
+            public String getMethod() {
+                return DavMethods.METHOD_REPORT;
+            }
+        };
+        method.setEntity(new StringEntity(buffer, ContentType.create("text/xml", "UTF-8")));
+
+        httpClient.executeDavRequest(method);
+    }
+
+    public void testCreateCalendar() throws IOException, DavException {
+        String folderName = "test & accentué";
+        //String folderName = "justatest";
+        String encodedFolderpath = URIUtil.encodePath("/users/" + session.getEmail() + "/calendar/" + folderName + '/');
+        // first delete calendar
+        session.deleteFolder("calendar/" + folderName);
+        String body =
+                "<?xml version=\"1.0\" encoding=\"utf-8\" ?>\n" +
+                        "   <C:mkcalendar xmlns:D=\"DAV:\"\n" +
+                        "                 xmlns:C=\"urn:ietf:params:xml:ns:caldav\">\n" +
+                        "     <D:set>\n" +
+                        "       <D:prop>\n" +
+                        "         <D:displayname>" + StringUtil.xmlEncode(folderName) + "</D:displayname>\n" +
+                        "         <C:calendar-description xml:lang=\"en\">Calendar description</C:calendar-description>\n" +
+                        "         <C:supported-calendar-component-set>\n" +
+                        "           <C:comp name=\"VEVENT\"/>\n" +
+                        "         </C:supported-calendar-component-set>\n" +
+                        "       </D:prop>\n" +
+                        "     </D:set>\n" +
+                        "   </C:mkcalendar>";
+        BaseDavRequest method = new BaseDavRequest(URI.create(encodedFolderpath)) {
+            @Override
+            public String getMethod() {
+                return "MKCALENDAR";
+            }
+
+            public boolean succeeded(HttpResponse response) {
+                int status = response.getStatusLine().getStatusCode();
+                return status == HttpStatus.SC_CREATED;
+            }
+        };
+        method.setEntity(new StringEntity(body, ContentType.create("text/xml", "UTF-8")));
+
+        httpClient.executeDavRequest(method);
+
+        HttpGet getRequest = new HttpGet(encodedFolderpath);
+        CloseableHttpResponse getResponse = httpClient.execute(getRequest);
+        assertEquals(org.apache.commons.httpclient.HttpStatus.SC_OK, getResponse.getStatusLine().getStatusCode());
+    }
+
+    public void testPropfindPrincipal() throws IOException, DavException {
+        //Settings.setLoggingLevel("httpclient.wire", Level.DEBUG);
+
+        DavPropertyNameSet davPropertyNameSet = new DavPropertyNameSet();
+        davPropertyNameSet.add(DavPropertyName.create("calendar-home-set", Namespace.getNamespace("urn:ietf:params:xml:ns:caldav")));
+        davPropertyNameSet.add(DavPropertyName.create("calendar-user-address-set", Namespace.getNamespace("urn:ietf:params:xml:ns:caldav")));
+        davPropertyNameSet.add(DavPropertyName.create("schedule-inbox-URL", Namespace.getNamespace("urn:ietf:params:xml:ns:caldav")));
+        davPropertyNameSet.add(DavPropertyName.create("schedule-outbox-URL", Namespace.getNamespace("urn:ietf:params:xml:ns:caldav")));
+        HttpPropfind method = new HttpPropfind("/principals/users/" + session.getEmail() + "/", davPropertyNameSet, 0);
+        MultiStatus multiStatus = httpClient.executeDavRequest(method);
+        MultiStatusResponse[] responses = multiStatus.getResponses();
+        assertEquals(1, responses.length);
+    }
+
+    public void testRenameCalendar() throws IOException, DavException {
+        String folderName = "testcalendarfolder";
+        String renamedFolderName = "renamedcalendarfolder";
+        String encodedFolderpath = URIUtil.encodePath("/users/" + session.getEmail() + "/calendar/" + folderName + "/");
+        // first delete calendar
+        session.deleteFolder("calendar/" + folderName);
+        session.deleteFolder("calendar/" + renamedFolderName);
+
+        session.createCalendarFolder("calendar/" + folderName, null);
+
+        DavPropertySet davPropertySet = new DavPropertySet();
+        davPropertySet.add(new DefaultDavProperty(DavPropertyName.create("displayname", Namespace.getNamespace("DAV:")), renamedFolderName));
+
+        HttpProppatch propPatchMethod = new HttpProppatch(URI.create(encodedFolderpath), davPropertySet, new DavPropertyNameSet());
+        httpClient.executeDavRequest(propPatchMethod);
+
+        ExchangeSession.Folder renamedFolder = session.getFolder("calendar/" + renamedFolderName);
+        assertNotNull(renamedFolder);
+
+    }
+
+    public void testWellKnown() throws IOException, DavException {
+        DavPropertyNameSet davPropertyNameSet = new DavPropertyNameSet();
+        davPropertyNameSet.add(DavPropertyName.create("current-user-principal", Namespace.getNamespace("DAV:")));
+        davPropertyNameSet.add(DavPropertyName.create("principal-URL", Namespace.getNamespace("DAV:")));
+        davPropertyNameSet.add(DavPropertyName.create("resourcetype", Namespace.getNamespace("DAV:")));
+        HttpPropfind method = new HttpPropfind("/.well-known/caldav", davPropertyNameSet, 0) {
+            @Override
+            public boolean succeeded(HttpResponse response) {
+                return response.getStatusLine().getStatusCode() == DavServletResponse.SC_MOVED_PERMANENTLY;
+            }
+        };
+        httpClient.executeDavRequest(method);
+    }
+
+    public void testPrincipalUrl() throws IOException, DavException {
+        DavPropertyNameSet davPropertyNameSet = new DavPropertyNameSet();
+        davPropertyNameSet.add(DavPropertyName.create("principal-URL", Namespace.getNamespace("DAV:")));
+        HttpPropfind method = new HttpPropfind("/principals/users/" + session.getEmail(), davPropertyNameSet, 0);
+        httpClient.executeDavRequest(method);
+    }
+
+    public void testPropfindRoot() throws IOException, DavException {
+        DavPropertyNameSet davPropertyNameSet = new DavPropertyNameSet();
+        davPropertyNameSet.add(DavPropertyName.create("current-user-principal", Namespace.getNamespace("DAV:")));
+        davPropertyNameSet.add(DavPropertyName.create("principal-URL", Namespace.getNamespace("DAV:")));
+        davPropertyNameSet.add(DavPropertyName.create("resourcetype", Namespace.getNamespace("DAV:")));
+        HttpPropfind method = new HttpPropfind("/", davPropertyNameSet, 0);
+        httpClient.executeDavRequest(method);
+    }
+
+    public void testPropfindPublicPrincipal() throws IOException, DavException {
+        DavPropertyNameSet davPropertyNameSet = new DavPropertyNameSet();
+        davPropertyNameSet.add(DavPropertyName.create("calendar-home-set", Namespace.getNamespace("urn:ietf:params:xml:ns:caldav")));
+        davPropertyNameSet.add(DavPropertyName.create("calendar-user-address-set", Namespace.getNamespace("urn:ietf:params:xml:ns:caldav")));
+        davPropertyNameSet.add(DavPropertyName.create("schedule-inbox-URL", Namespace.getNamespace("urn:ietf:params:xml:ns:caldav")));
+        davPropertyNameSet.add(DavPropertyName.create("schedule-outbox-URL", Namespace.getNamespace("urn:ietf:params:xml:ns:caldav")));
+        HttpPropfind method = new HttpPropfind("/principals/public/testcalendar/", davPropertyNameSet, 0);
+        MultiStatus multiStatus = httpClient.executeDavRequest(method);
+        MultiStatusResponse[] responses = multiStatus.getResponses();
+        assertEquals(1, responses.length);
+    }
+
+    public void testInvalidDavRequest() throws IOException {
         BaseDavRequest method = new BaseDavRequest(URI.create("/users/" + session.getEmail() + "/calendar/")) {
             @Override
             public String getMethod() {
