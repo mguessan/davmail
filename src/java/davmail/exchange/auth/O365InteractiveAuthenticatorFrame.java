6553fe02edcc9d5c64c7725ec8a653d5f661cd2e
Mickaël Guessant <mguessan@free.fr>
2019-02-23 17:55:56 +0000

    EWS: fix support for new Okta authentication form, need to disable integrity check
    git-svn-id: https://svn.code.sf.net/p/davmail/code/trunk@2977 3d1905a2-6b24-0410-a738-b14d5a86fcbd

--- src/java/davmail/exchange/auth/O365InteractiveAuthenticatorFrame.java
+++ src/java/davmail/exchange/auth/O365InteractiveAuthenticatorFrame.java
@@ -88,12 +88,16 @@ public class O365InteractiveAuthenticatorFrame extends JFrame {
                                     LOGGER.warn("Failed: form calls watson");
                                 }
                                 final HttpURLConnection httpsURLConnection = (HttpURLConnection) super.openConnection(url, proxy);
+                                httpsURLConnection.setRequestProperty("User-Agent", DavGatewayHttpClientFacade.getUserAgent());
+
                                 if (("login.microsoftonline.com".equals(url.getHost()) && "/common/oauth2/authorize".equals(url.getPath()))
                                     || ("login.live.com".equals(url.getHost()) && "/oauth20_authorize.srf".equals(url.getPath()))
                                     || ("login.live.com".equals(url.getHost()) && "/ppsecure/post.srf".equals(url.getPath()))
                                     || ("login.microsoftonline.com".equals(url.getHost()) && "/login.srf".equals(url.getPath()))
                                     || ("login.microsoftonline.com".equals(url.getHost()) && "/common/login".equals(url.getPath()))
                                     || ("login.microsoftonline.com".equals(url.getHost()) && "/common/SAS/ProcessAuth".equals(url.getPath()))
+                                    // Okta authentication form
+                                    || (url.getHost().endsWith(".okta.com") && "/login/login.htm".equals(url.getPath()))
                                 ) {
                                     LOGGER.debug("Disable integrity check on external resources");
 
