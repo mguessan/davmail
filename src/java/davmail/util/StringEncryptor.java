6d4b66419d5fec2573ff192edc8cd83c1ff017e8
Mickaël Guessant <mguessan@free.fr>
2019-09-11 16:23:19 +0000

    Implement password based string encryptor
    git-svn-id: https://svn.code.sf.net/p/davmail/code/trunk@3089 3d1905a2-6b24-0410-a738-b14d5a86fcbd

---
+++ src/java/davmail/util/StringEncryptor.java
@@ -0,0 +1,97 @@
+/*
+ * DavMail POP/IMAP/SMTP/CalDav/LDAP Exchange Gateway
+ * Copyright (C) 2010  Mickael Guessant
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package davmail.util;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.PBEParameterSpec;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.InetAddress;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
+/**
+ * Encrypt string with user password.
+ * Simple implementation based on AES
+ */
+public class StringEncryptor {
+
+    static final String ALGO = "PBEWithHmacSHA256AndAES_128";
+    static String fingerprint;
+
+    static {
+        try {
+            fingerprint = InetAddress.getLocalHost().getHostName().substring(0, 16);
+        } catch (Throwable t) {
+            fingerprint = "davmailgateway!&";
+        }
+    }
+
+    private final String password;
+
+    public StringEncryptor(String password) {
+        this.password = password;
+    }
+
+    public String encryptString(String value) throws IOException {
+        try {
+            byte[] plaintext = value.getBytes("UTF-8");
+
+            // Encrypt
+            Cipher enc = Cipher.getInstance(ALGO);
+            enc.init(Cipher.ENCRYPT_MODE, getSecretKey(), getPBEParameterSpec());
+            byte[] encrypted = enc.doFinal(plaintext);
+            return IOUtil.encodeBase64AsString(encrypted);
+
+        } catch (Exception e) {
+            throw new IOException(e);
+        }
+    }
+
+    public String decryptString(String value) throws IOException {
+        try {
+            byte[] encrypted = IOUtil.decodeBase64(value);
+
+            Cipher dec = Cipher.getInstance(ALGO);
+            dec.init(Cipher.DECRYPT_MODE, getSecretKey(), getPBEParameterSpec());
+            byte[] decrypted = dec.doFinal(encrypted);
+            return new String(decrypted, "UTF-8");
+
+        } catch (Exception e) {
+            throw new IOException(e);
+        }
+    }
+
+    private SecretKey getSecretKey() throws InvalidKeySpecException, NoSuchAlgorithmException {
+        PBEKeySpec keySpec = new PBEKeySpec(password.toCharArray());
+
+        SecretKeyFactory kf = SecretKeyFactory.getInstance(ALGO);
+        return kf.generateSecret(keySpec);
+    }
+
+    private PBEParameterSpec getPBEParameterSpec() throws UnsupportedEncodingException {
+        byte[] bytes = fingerprint.getBytes("UTF-8");
+        return new PBEParameterSpec(bytes, 10000, new IvParameterSpec(bytes));
+    }
+}
