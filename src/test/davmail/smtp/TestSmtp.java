b12900a59d9f4ff5afcf57a44feff5704c2038e3
Mickaël Guessant <mguessan@free.fr>
2019-12-18 07:54:22 +0000

    SMTP: fix smtp test cases
    git-svn-id: https://svn.code.sf.net/p/davmail/code/trunk@3239 3d1905a2-6b24-0410-a738-b14d5a86fcbd

--- src/test/davmail/smtp/TestSmtp.java
+++ src/test/davmail/smtp/TestSmtp.java
@@ -24,19 +24,24 @@ import davmail.Settings;
 import davmail.exchange.DoubleDotOutputStream;
 import davmail.exchange.ExchangeSession;
 import davmail.exchange.ExchangeSessionFactory;
-import davmail.util.IOUtil;
 import org.apache.commons.codec.binary.Base64;
-import org.apache.log4j.Level;
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
 
 import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.internet.MimeMessage;
-import java.io.*;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Test smtp send message.
  */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TestSmtp extends AbstractDavMailTestCase {
     static Socket clientSocket;
     static BufferedOutputStream socketOutputStream;
@@ -47,7 +52,7 @@ public class TestSmtp extends AbstractDavMailTestCase {
     }
 
     protected void write(String line) throws IOException {
-        socketOutputStream.write(line.getBytes("ASCII"));
+        socketOutputStream.write(line.getBytes(StandardCharsets.US_ASCII));
         socketOutputStream.flush();
     }
 
@@ -78,7 +83,7 @@ public class TestSmtp extends AbstractDavMailTestCase {
                 baos.write(character);
             }
         }
-        return new String(baos.toByteArray(), "ASCII");
+        return new String(baos.toByteArray(), StandardCharsets.US_ASCII);
     }
 
     @Override
@@ -111,7 +116,35 @@ public class TestSmtp extends AbstractDavMailTestCase {
         sendAndCheckMessage(mimeMessage, null, bcc);
     }
 
+
     public void sendAndCheckMessage(MimeMessage mimeMessage, String from, String bcc) throws IOException, MessagingException, InterruptedException {
+        sendMessage(mimeMessage, from, bcc);
+        assertEquals("250 Queued mail for delivery", readLine());
+        ExchangeSession.Message sentMessage = getSentMessage(mimeMessage.getMessageID());
+        assertEquals(mimeMessage.getDataHandler().getContent(), sentMessage.getMimeMessage().getDataHandler().getContent());
+    }
+
+    private ExchangeSession.Message getSentMessage(String messageId) throws IOException, MessagingException, InterruptedException {
+        // wait for asynchronous message send
+        ExchangeSession.MessageList messages = null;
+        for (int i = 0; i < 5; i++) {
+            messages = session.searchMessages("Sent", session.headerIsEqualTo("references", messageId));
+            if (messages.size() > 0) {
+                break;
+            }
+            Thread.sleep(1000);
+        }
+        assertEquals(1, messages.size());
+        ExchangeSession.Message sentMessage = messages.get(0);
+        sentMessage.getMimeMessage().writeTo(System.out);
+        return sentMessage;
+    }
+
+    public void sendMessage(MimeMessage mimeMessage) throws IOException, MessagingException {
+        sendMessage(mimeMessage, null, null);
+    }
+
+    public void sendMessage(MimeMessage mimeMessage, String from, String bcc) throws IOException, MessagingException {
         // generate message id
         mimeMessage.saveChanges();
         // mimeMessage.writeTo(System.out);
@@ -135,24 +168,10 @@ public class TestSmtp extends AbstractDavMailTestCase {
         mimeMessage.writeTo(new DoubleDotOutputStream(socketOutputStream));
         writeLine("");
         writeLine(".");
-        assertEquals("250 Queued mail for delivery", readLine());
-        // wait for asynchronous message send
-        ExchangeSession.MessageList messages = null;
-        for (int i = 0; i < 5; i++) {
-            messages = session.searchMessages("Sent", session.headerIsEqualTo("references", mimeMessage.getMessageID()));
-            if (messages.size() > 0) {
-                break;
-            }
-            Thread.sleep(1000);
-        }
-        assertEquals(1, messages.size());
-        ExchangeSession.Message sentMessage = messages.get(0);
-        sentMessage.getMimeMessage().writeTo(System.out);
-        assertEquals(mimeMessage.getDataHandler().getContent(), sentMessage.getMimeMessage().getDataHandler().getContent());
     }
 
     public void testSendSimpleMessage() throws IOException, MessagingException, InterruptedException {
-        String body = "Test message";
+        String body = "Test message\r\n";
         MimeMessage mimeMessage = new MimeMessage((Session) null);
         mimeMessage.addHeader("From", session.getEmail());
         mimeMessage.addHeader("To", Settings.getProperty("davmail.to"));
@@ -161,14 +180,15 @@ public class TestSmtp extends AbstractDavMailTestCase {
         sendAndCheckMessage(mimeMessage);
     }
 
-    public void testInvalidFrom() throws IOException, MessagingException, InterruptedException {
+    public void testInvalidFrom() throws IOException, MessagingException {
         String body = "Test message";
         MimeMessage mimeMessage = new MimeMessage((Session) null);
         mimeMessage.addHeader("From", "guessant@loca.net");
         mimeMessage.addHeader("To", Settings.getProperty("davmail.to"));
         mimeMessage.setSubject("Test subject");
         mimeMessage.setText(body);
-        sendAndCheckMessage(mimeMessage, "guessant@loca.net", null);
+        sendMessage(mimeMessage, "guessant@loca.net", null);
+        assertEquals("451", readLine().substring(0, 3));
     }
 
     public void testSendMessage() throws IOException, MessagingException, InterruptedException {
@@ -186,12 +206,12 @@ public class TestSmtp extends AbstractDavMailTestCase {
         MimeMessage mimeMessage = new MimeMessage((Session) null);
         mimeMessage.addHeader("to", Settings.getProperty("davmail.to"));
         mimeMessage.setSubject("Test subject dav");
-        mimeMessage.setText("Test message");
+        mimeMessage.setText("Test message\r\n");
         sendAndCheckMessage(mimeMessage, Settings.getProperty("davmail.bcc"));
     }
 
     public void testDotMessage() throws IOException, MessagingException, InterruptedException {
-        String body = "First line\r\n.\r\nSecond line";
+        String body = "First line\r\n.\r\nSecond line\r\n";
         MimeMessage mimeMessage = new MimeMessage((Session) null);
         mimeMessage.addHeader("to", Settings.getProperty("davmail.to"));
         mimeMessage.setSubject("Test subject");
@@ -201,7 +221,7 @@ public class TestSmtp extends AbstractDavMailTestCase {
 
     public void testSendMessageTwice() throws IOException, MessagingException, InterruptedException {
         Settings.setProperty("davmail.smtpCheckDuplicates", "true");
-        String body = "First line\r\n.\r\nSecond line";
+        String body = "First line\r\n.\r\nSecond line\r\n";
         MimeMessage mimeMessage = new MimeMessage((Session) null);
         mimeMessage.addHeader("to", Settings.getProperty("davmail.to"));
         mimeMessage.setSubject("Test subject");
@@ -211,7 +231,7 @@ public class TestSmtp extends AbstractDavMailTestCase {
     }
 
     public void testComplexToMessage() throws IOException, MessagingException, InterruptedException {
-        String body = "Test message";
+        String body = "Test message\r\n";
         MimeMessage mimeMessage = new MimeMessage((Session) null);
         mimeMessage.addHeader("To", "nickname <" + Settings.getProperty("davmail.to") + '>');
         mimeMessage.setSubject("Test subject");
@@ -220,7 +240,7 @@ public class TestSmtp extends AbstractDavMailTestCase {
     }
 
     public void testSendPlainTextMessage() throws IOException, MessagingException, InterruptedException {
-        String body = "Test plain text message";
+        String body = "Test plain text message\r\n";
         MimeMessage mimeMessage = new MimeMessage((Session) null);
         mimeMessage.addHeader("To", Settings.getProperty("davmail.to"));
         mimeMessage.setSubject("Test text/plain message");
@@ -229,23 +249,23 @@ public class TestSmtp extends AbstractDavMailTestCase {
     }
 
     public void testSendHtmlMessage() throws IOException, MessagingException, InterruptedException {
-        String body = "Test html message <font color=\"#ff0000\">red</font>";
+        String body = "Test html message <font color=\"#ff0000\">red</font>\r\n";
         MimeMessage mimeMessage = new MimeMessage((Session) null);
         mimeMessage.addHeader("To", Settings.getProperty("davmail.to"));
         mimeMessage.setSubject("Test html message");
         mimeMessage.setContent(body, "text/html");
-        sendAndCheckMessage(mimeMessage);
+        sendMessage(mimeMessage);
+        assertEquals("250 Queued mail for delivery", readLine());
+        ExchangeSession.Message sentMessage = getSentMessage(mimeMessage.getMessageID());
+        // Office 365 adds html prefix
+        assertEquals("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=us-ascii\">"+mimeMessage.getDataHandler().getContent(), sentMessage.getMimeMessage().getDataHandler().getContent());
+
     }
 
-    public void testQuit() throws IOException {
+    public void test_ZZ_Quit() throws IOException {
         writeLine("QUIT");
         assertEquals("221 Closing connection", readLine());
     }
 
-
-    public void testBrokenMessage() throws MessagingException, IOException, InterruptedException {
-        MimeMessage mimeMessage = new MimeMessage(null, new FileInputStream("test.eml"));
-        sendAndCheckMessage(mimeMessage);
-    }
 
 }
