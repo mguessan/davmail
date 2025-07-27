6a9e5c61f57f4ec7ba17ad12d2777cb8fb9324a5
Mickaël Guessant <mguessan@free.fr>
2025-07-27 16:38:39 +0000

    Graph: switch back to custom scopes
    git-svn-id: https://svn.code.sf.net/p/davmail/code/trunk@3743 3d1905a2-6b24-0410-a738-b14d5a86fcbd

--- src/java/davmail/exchange/auth/O365Authenticator.java
+++ src/java/davmail/exchange/auth/O365Authenticator.java
@@ -87,10 +87,10 @@ public class O365Authenticator implements ExchangeAuthenticator {
                 if (Settings.getBooleanProperty("davmail.enableOidc", false)) {
                     // OIDC compliant
                     uriBuilder.setPath("/" + tenantId + "/oauth2/v2.0/authorize")
-                            //.addParameter("scope", "openid profile offline_access Mail.ReadWrite Calendars.ReadWrite MailboxSettings.Read Mail.ReadWrite.Shared Contacts.ReadWrite Tasks.ReadWrite Mail.Send");
+                            .addParameter("scope", "openid profile offline_access Mail.ReadWrite Calendars.ReadWrite MailboxSettings.Read Mail.ReadWrite.Shared Contacts.ReadWrite Tasks.ReadWrite Mail.Send");
                     // return scopes with 00000003-0000-0000-c000-000000000000 scopes
                     //.addParameter("scope", "openid profile offline_access .default");
-                    .addParameter("scope", "openid profile offline_access "+Settings.getGraphUrl()+"/.default");
+                    //.addParameter("scope", "openid profile offline_access "+Settings.getGraphUrl()+"/.default");
                     //.addParameter("scope", "openid " + Settings.getOutlookUrl() + "/EWS.AccessAsUser.All AuditLog.Read.All Calendar.ReadWrite Calendars.Read.Shared Calendars.ReadWrite Contacts.ReadWrite DataLossPreventionPolicy.Evaluate Directory.AccessAsUser.All Directory.Read.All Files.Read Files.Read.All Files.ReadWrite.All Group.Read.All Group.ReadWrite.All InformationProtectionPolicy.Read Mail.ReadWrite Mail.Send Notes.Create Organization.Read.All People.Read People.Read.All Printer.Read.All PrintJob.ReadWriteBasic SensitiveInfoType.Detect SensitiveInfoType.Read.All SensitivityLabel.Evaluate Tasks.ReadWrite TeamMember.ReadWrite.All TeamsTab.ReadWriteForChat User.Read.All User.ReadBasic.All User.ReadWrite Users.Read");
                 } else {
                     // Outlook desktop relies on classic authorize endpoint
