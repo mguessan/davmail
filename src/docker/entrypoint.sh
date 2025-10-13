954af918bef8ef50d49d9356566482bba0928e9e
Mickaël Guessant <mguessan@free.fr>
2025-10-13 08:08:16 +0000

    Docker: review classpath, separate openjfx modules configuration, introduce DAVMAIL_PROPERTIES env variable
    git-svn-id: https://svn.code.sf.net/p/davmail/code/trunk@3817 3d1905a2-6b24-0410-a738-b14d5a86fcbd

--- src/docker/entrypoint.sh
+++ src/docker/entrypoint.sh
@@ -3,15 +3,17 @@
 # davmail.properties is in /davmail.properties
 # /home is home directory
 #
-# set memory and enable DNS expiration
 
-: "${JAVA_OPT_BASE:=-Xmx512M -Dsun.net.inetaddr.ttl=60}"
+: "${DAVMAIL_PROPERTIES:=/config/davmail.properties}"
+
+# enable DNS expiration
+: "${JAVA_OPT_BASE:=-Dsun.net.inetaddr.ttl=60}"
 : "${JAVA_OPT_EXPORTS:=}"
-JAVA_OPTS="$JAVA_OPT_BASE $JAVA_OPT_EXPORTS $JAVA_OPT_JAR $JAVA_OPT_USER"
+: "${JAVA_OPT_FX:=--module-path /usr/share/java/javafx-base.jar:/usr/share/java/javafx-controls.jar:/usr/share/java/javafx-fxml.jar:/usr/share/java/javafx-graphics.jar:/usr/share/java/javafx-media.jar:/usr/share/java/javafx-swing.jar:/usr/share/java/javafx-web.jar --add-modules=javafx.base,javafx.controls,javafx.fxml,javafx.graphics,javafx.media,javafx.swing,javafx.web}"
+JAVA_OPTS="$JAVA_OPT_BASE $JAVA_OPT_EXPORTS $JAVA_OPT_FX $JAVA_OPT_JAR $JAVA_OPT_USER"
 
 # Determined experimentally
-export CLASSPATH=/davmail/davmail.jar:/usr/share/java/commons-logging.jar:/usr/share/java/httpclient.jar:/usr/share/java/httpcore.jar:/usr/share/java/jackrabbit-webdav.jar:/usr/share/java/javafx-base.jar:/usr/share/java/javafx-controls.jar:/usr/share/java/javafx-graphics.jar:/usr/share/java/javafx-media.jar:/usr/share/java/javafx-swing.jar:/usr/share/java/javafx-web.jar:/usr/share/java/javax.mail.jar:/usr/share/java/jettison.jar:/usr/share/java/jna.jar:/usr/share/java/log4j-1.2.jar:/usr/share/java/swt4.jar:/usr/share/java/stax2-api.jar:xercesImpl.jar:woodstox-core-asl.jar:commons-codec.jar:htmlcleaner.jar:jdom2.jar:jcifs.jar
-export SWT_GTK3=0
+export CLASSPATH=/usr/share/java/commons-codec.jar:/usr/share/java/commons-logging.jar:/usr/share/java/htmlcleaner.jar:/usr/share/java/httpclient.jar:/usr/share/java/httpcore.jar:/usr/share/java/jackrabbit-webdav.jar:/usr/share/java/jcifs.jar:/usr/share/java/jettison.jar:/usr/share/java/log4j-1.2.jar:/usr/share/java/javax.mail.jar:/usr/share/java/slf4j-api.jar:/usr/share/java/slf4j-log4j12.jar:/usr/share/java/stax2-api.jar:/usr/share/java/woodstox-core.jar:/usr/share/java/stax2-api.jar:/usr/share/java/slf4j-jcl.jar:/usr/share/java/jdom2.jar:/usr/share/java/javax.activation.jar
 
 if [ -z "$1" ]; then set -- /config/davmail.properties -notray; fi
 
