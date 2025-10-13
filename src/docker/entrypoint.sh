faf7bd7d501e93de1246d95eff98ab4d9662c4ae
Mickaël Guessant <mguessan@free.fr>
2025-10-13 08:09:34 +0000

    Docker: default option is notray
    git-svn-id: https://svn.code.sf.net/p/davmail/code/trunk@3818 3d1905a2-6b24-0410-a738-b14d5a86fcbd

--- src/docker/entrypoint.sh
+++ src/docker/entrypoint.sh
@@ -15,7 +15,8 @@ JAVA_OPTS="$JAVA_OPT_BASE $JAVA_OPT_EXPORTS $JAVA_OPT_FX $JAVA_OPT_JAR $JAVA_OPT
 # Determined experimentally
 export CLASSPATH=/usr/share/java/commons-codec.jar:/usr/share/java/commons-logging.jar:/usr/share/java/htmlcleaner.jar:/usr/share/java/httpclient.jar:/usr/share/java/httpcore.jar:/usr/share/java/jackrabbit-webdav.jar:/usr/share/java/jcifs.jar:/usr/share/java/jettison.jar:/usr/share/java/log4j-1.2.jar:/usr/share/java/javax.mail.jar:/usr/share/java/slf4j-api.jar:/usr/share/java/slf4j-log4j12.jar:/usr/share/java/stax2-api.jar:/usr/share/java/woodstox-core.jar:/usr/share/java/stax2-api.jar:/usr/share/java/slf4j-jcl.jar:/usr/share/java/jdom2.jar:/usr/share/java/javax.activation.jar
 
-if [ -z "$1" ]; then set -- /config/davmail.properties -notray; fi
+# default option is notray
+if [ -z "$1" ]; then set -- -notray; fi
 
 # shellcheck disable=SC2086
 exec "${JAVA:-java}" $JAVA_OPTS davmail.DavGateway "$@"
