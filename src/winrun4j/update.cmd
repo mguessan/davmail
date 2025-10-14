7915bbea91f6852be135b529cbaf1be75daf175e
Mickaël Guessant <mguessan@free.fr>
2025-10-14 09:18:53 +0000

    Build: remove ini configuration from winrun4j wrappers (no need to update exe for next lib upgrade)
    git-svn-id: https://svn.code.sf.net/p/davmail/code/trunk@3839 3d1905a2-6b24-0410-a738-b14d5a86fcbd

--- src/winrun4j/update.cmd
+++ src/winrun4j/update.cmd
@@ -1,10 +1,10 @@
 @echo off
 rcedit64 /C davmail64.exe
 rcedit64 /I davmail64.exe davmail.ico
-rcedit64 /N davmail64.exe davmail64.ini
+@rem rcedit64 /N davmail64.exe davmail64.ini
 
 rcedit64 /C davmailservice64.exe
 rcedit64 /I davmailservice64.exe davmail.ico
-rcedit64 /N davmailservice64.exe davmailservice64.ini
+@rem rcedit64 /N davmailservice64.exe davmailservice64.ini
 
 ie4uinit.exe -show
