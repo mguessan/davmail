24160d0209d34c86bcc3290f579a9d7b6a82b7fc
Mickaël Guessant <mguessan@free.fr>
2026-09-14 08:11:53 +0000

    Graph: merge https://github.com/mguessan/davmail/pull/517 keep origin of appended messages (date, Received, threading, read flag)
    git-svn-id: https://svn.code.sf.net/p/davmail/code/trunk@4398 3d1905a2-6b24-0410-a738-b14d5a86fcbd

--- src/java/davmail/exchange/graph/GraphExchangeSession.java
+++ src/java/davmail/exchange/graph/GraphExchangeSession.java
@@ -74,6 +74,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -2292,9 +2293,13 @@ public class GraphExchangeSession extends ExchangeSession {
 
                 // unset draft flag on returned draft message properties
                 graphResponse.put("messageFlags", "4");
-                // clear read flag by default
-                graphResponse.put("read", false);
+                // read flag travels in the draft property (1 = read, see ImapConnection)
+                graphResponse.put("read", properties != null && "1".equals(properties.get("draft")));
                 applyMessageProperties(graphResponse, properties);
+                // the recreated message would otherwise carry the upload time and no
+                // Received: headers: Graph ignores receivedDateTime, sentDateTime and
+                // internetMessageHeaders on creation, but honours the MAPI properties
+                preserveMimeOrigin(graphResponse, mimeMessage, properties);
 
                 // now use this to recreate message in the right folder
                 graphResponse = executeGraphRequest(new GraphRequestBuilder()
@@ -2324,6 +2329,80 @@ public class GraphExchangeSession extends ExchangeSession {
                 .setMailbox(folderId.mailbox)
                 .setObjectId(graphResponse.optString("id"))
                 .setSelectFields(IMAP_MESSAGE_ATTRIBUTES)));
+    }
+
+    /**
+     * Copy delivery time, submit time and transport headers of an appended MIME message
+     * to the MAPI properties Graph accepts on creation, so the recreated message keeps
+     * its original Date, INTERNALDATE, Received: and threading headers.
+     *
+     * @param graphResponse message properties about to be posted
+     * @param mimeMessage   appended MIME message
+     * @param properties    IMAP APPEND properties (datereceived is the INTERNALDATE)
+     * @throws JSONException on JSON error
+     */
+    private void preserveMimeOrigin(GraphObject graphResponse, MimeMessage mimeMessage, Map<String, String> properties) throws JSONException {
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.ENGLISH);
+        dateFormatter.setTimeZone(GMT_TIMEZONE);
+        Date sentDate = null;
+        try {
+            sentDate = mimeMessage.getSentDate();
+        } catch (MessagingException e) {
+            LOGGER.warn("Unable to read sent date of appended message: " + e.getMessage());
+        }
+
+        String deliveryTime = properties == null ? null : properties.get("datereceived");
+        if (deliveryTime == null && sentDate != null) {
+            deliveryTime = dateFormatter.format(sentDate);
+        }
+        if (deliveryTime != null) {
+            graphResponse.put("deliverytime", deliveryTime);
+        }
+        if (sentDate != null) {
+            graphResponse.put("clientsubmittime", dateFormatter.format(sentDate));
+        }
+        StringBuilder headers = new StringBuilder();
+        Enumeration<String> headerLines = null;
+        try {
+            headerLines = mimeMessage.getAllHeaderLines();
+        } catch (MessagingException e) {
+            LOGGER.warn("Unable to read header lines of appended message: " + e.getMessage());
+        }
+        if (headerLines != null) {
+            while (headerLines.hasMoreElements()) {
+                headers.append(headerLines.nextElement()).append("\r\n");
+            }
+        }
+        if (headers.length() > 0) {
+            graphResponse.put("messageheaders", headers.toString());
+        }
+        // threading headers are not among those Exchange rebuilds from the
+        // transport headers, but have MAPI properties of their own
+        String inReplyTo = getMessageHeader(mimeMessage, "In-Reply-To");
+        if (inReplyTo != null) {
+            graphResponse.put("inreplyto", inReplyTo.trim());
+        }
+        String references = getMessageHeader(mimeMessage, "References");
+        if (references != null) {
+            graphResponse.put("references", references.trim());
+        }
+    }
+
+    /**
+     * Get message header value from MIME message.
+     * Warn on error, do not throw exception.
+     * @param mimeMessage MIME message
+     * @param headerName header name
+     * @return header value
+     */
+    protected String getMessageHeader(MimeMessage mimeMessage, String headerName) {
+        String headerValue = null;
+        try {
+            headerValue = mimeMessage.getHeader(headerName, " ");
+        } catch (MessagingException e) {
+            LOGGER.warn("Unable to read header '" + headerName + "' of appended message: " + e.getMessage());
+        }
+        return headerValue;
     }
 
     protected Message getMessage(FolderId folderId, String messageId) throws IOException {
