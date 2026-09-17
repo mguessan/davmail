c7f8abb2809bf96ff4b078b743bcafae059d6776
Mickaël Guessant <mguessan@free.fr>
2026-09-17 11:23:16 +0000

    Graph: fix DavMailAuthenticationException check in O365Token, fail when Mail.ReadWrite is missing instead of EWS.AccessAsUser.All present, see https://github.com/mguessan/davmail/issues/518
    git-svn-id: https://svn.code.sf.net/p/davmail/code/trunk@4400 3d1905a2-6b24-0410-a738-b14d5a86fcbd

--- src/java/davmail/exchange/auth/O365Token.java
+++ src/java/davmail/exchange/auth/O365Token.java
@@ -139,8 +139,8 @@ public class O365Token {
             scope = jsonToken.optString("scope", null);
             LOGGER.debug("Obtained token for scopes: " + scope);
 
-            if (Settings.isGraphEnabled() && scope != null && scope.contains("EWS.AccessAsUser.All")) {
-                throw new DavMailAuthenticationException("LOG_MESSAGE", "Received a token with EWS.AccessAsUser.All for a graph session");
+            if (Settings.isGraphEnabled() && scope != null && !scope.contains("Mail.ReadWrite")) {
+                throw new DavMailAuthenticationException("LOG_MESSAGE", "Received a token without Mail.ReadWrite for a graph session");
             }
             // access token expires after one hour
             accessToken = jsonToken.getString("access_token");
